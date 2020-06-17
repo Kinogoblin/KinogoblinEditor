2e6eb29866a74876174a38926bf36a68e8fc2a80
kinogoblin <kinogoblin@gmail.com>
2020-06-17 13:21:41 +0300

    Update KinogoblinEditor.cs

--- Editor/KinogoblinEditor.cs
+++ Editor/KinogoblinEditor.cs
@@ -76,6 +76,7 @@
         private static Texture2D m_Logo = null;
         void OnEnable()
         {
+            Helpful.Debug(logoPath);
             if (!File.Exists(logoPath))
             {
                 logoPath = Application.dataPath + "/GitKinogoblin/KinogoblinEditor/Editor/Icons/Logo.png";
