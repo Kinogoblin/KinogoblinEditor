583cce2b9a96b7712e9b25bbd19500ca829a9ba5
Kinogoblin <kinogoblin@gmail.com>
2021-08-31 18:50:19 +0300

    add OnPostprocessGameObjectWithUserProperties

---
+++ Runtime/UserDataHolder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kinogoblin.Runtime
+{
+    public class UserDataHolder : MonoBehaviour
+    {
+        public List<UserDataFromModel> UserData = new List<UserDataFromModel>();
+    }
+
+    [System.Serializable]
+    public class UserDataFromModel
+    {
+        public string name;
+        public string type;
+        public string value;
+    }
+}
